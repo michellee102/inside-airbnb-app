ba23dbbf238594bbeeba400254f881b73d5e510d
Michelle <93997041+michellee102@users.noreply.github.com>
2024-05-02 17:18:00 +0200

    frontend start

--- InsideAirbnb_MVC_App/InsideAirbnb_MVC_App/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InsideAirbnb_MVC_App.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
